97558b4b3826fe8dd4fe5dbcb355d53c7860c5ce
audayasree96 <udayasree1947@gmail.com>
2023-07-30 13:21:24 +0530

    code

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h1 className="text-center">All Right Reserved &copy; MNPL</h1>
+      <p className="text-center mt-3">
+        
+        
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
